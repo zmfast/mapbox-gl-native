e8cd4b049826f45464322b1e29058a21b65b035c
tobrun <tobrun.van.nuland@gmail.com>
2019-04-25 11:32:37 +0200

    [android] - example of symbol sort key

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/style/CustomSpriteActivity.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/style/CustomSpriteActivity.java
@@ -1,38 +1,31 @@
 package com.mapbox.mapboxsdk.testapp.activity.style;
 
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
+import com.google.gson.JsonObject;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.expressions.Expression;
 import com.mapbox.mapboxsdk.style.layers.Layer;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import com.mapbox.mapboxsdk.testapp.R;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import timber.log.Timber;
-
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.*;
 
 /**
  * Test activity showcasing adding a sprite image and use it in a Symbol Layer
  */
 public class CustomSpriteActivity extends AppCompatActivity {
-
-  private static final String CUSTOM_ICON = "custom-icon";
 
   private GeoJsonSource source;
   private MapboxMap mapboxMap;
@@ -50,52 +43,40 @@ public class CustomSpriteActivity extends AppCompatActivity {
       mapboxMap = map;
 
       map.setStyle(Style.MAPBOX_STREETS, style -> {
-        final FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setColorFilter(ContextCompat.getColor(CustomSpriteActivity.this, R.color.primary));
-        fab.setOnClickListener(new View.OnClickListener() {
+        List<Feature> featureList = new ArrayList<>();
 
-          private Point point;
+        LatLng[] latLngs = new LatLng[] {
+          new LatLng(52.518635, 13.400857),
+          new LatLng(52.518485, 13.401109),
+          new LatLng(52.518666, 13.400852),
+          new LatLng(52.518660, 13.401920),
+          new LatLng(52.518751, 13.401626)
+        };
 
-          @Override
-          public void onClick(View view) {
-            if (point == null) {
-              Timber.i("First click -> Car");
-              // Add an icon to reference later
-              style.addImage(CUSTOM_ICON, BitmapFactory.decodeResource(getResources(), R.drawable.ic_car_top));
+        for (int i = 0; i < latLngs.length; i++) {
+          JsonObject jsonObject = new JsonObject();
+          jsonObject.addProperty("title", String.valueOf(i));
+          featureList.add(Feature.fromGeometry(Point.fromLngLat(latLngs[i].getLongitude(), latLngs[i].getLatitude()), jsonObject));
+        }
 
-              // Add a source with a geojson point
-              point = Point.fromLngLat(13.400972d, 52.519003d);
-              source = new GeoJsonSource(
-                "point",
-                FeatureCollection.fromFeatures(new Feature[] {Feature.fromGeometry(point)})
-              );
-              mapboxMap.getStyle().addSource(source);
 
-              // Add a symbol layer that references that point source
-              layer = new SymbolLayer("layer", "point");
-              layer.setProperties(
-                // Set the id of the sprite to use
-                iconImage(CUSTOM_ICON),
-                iconAllowOverlap(true),
-                iconIgnorePlacement(true)
-              );
+        source = new GeoJsonSource("source", FeatureCollection.fromFeatures(featureList));
+        style.addSource(source);
 
-              // lets add a circle below labels!
-              mapboxMap.getStyle().addLayerBelow(layer, "waterway-label");
+        style.addImage("ic_launcher_round", BitmapUtils.getDrawableFromRes(getApplicationContext(), R.drawable.ic_launcher_round));
 
-              fab.setImageResource(R.drawable.ic_directions_car_black);
-            } else {
-              // Update point
-              point = Point.fromLngLat(point.longitude() + 0.001,
-                point.latitude() + 0.001);
-              source.setGeoJson(FeatureCollection.fromFeatures(new Feature[] {Feature.fromGeometry(point)}));
-
-              // Move the camera as well
-              mapboxMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(
-                point.latitude(), point.longitude())));
-            }
-          }
-        });
+        style.addLayer(new SymbolLayer("layer", "source")
+          .withProperties(
+            iconAllowOverlap(true),
+            textAllowOverlap(true),
+            iconIgnorePlacement(true),
+            textIgnorePlacement(true),
+            textPadding(4.0f),
+            symbolSortKey(5.0f),
+            textField(Expression.get("title")),
+            iconImage("ic_launcher_round")
+          )
+        );
       });
     });
   }
